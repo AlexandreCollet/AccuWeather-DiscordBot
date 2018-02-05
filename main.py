8d566e6f90c13c38aadb8cedd33896d0191d6bcf
Alexandre Collet <alexandre@collet.email>
2018-02-05 21:39:41 +0100

    Create settings.py file

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
-import os
-
 from discord.ext import commands
 
+import settings
 from accuweather_command import AccuWeatherCommand
 
 
-discord_login_token = os.environ.get('ACCUWEATHER_DISCORDBOT_DISCORD_TOKEN')
-accuweather_api_key = os.environ.get('ACCUWEATHER_API_KEY')
-language = os.environ.get('ACCUWEATHER_DISCORDBOT_LANGUAGE', None)
-
 bot = commands.Bot(command_prefix=commands.when_mentioned_or('$'))
-bot.add_cog(AccuWeatherCommand(bot, accuweather_api_key, language))
+bot.add_cog(AccuWeatherCommand(bot, settings.ACCUWEATHER_API_KEY, settings.LANGUAGE))
 
 @bot.event
 async def on_ready():
     print('Bot launched')
 
-bot.run(discord_login_token)
+bot.run(settings.DISCORD_LOGIN_TOKEN)
