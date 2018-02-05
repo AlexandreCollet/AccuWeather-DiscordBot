80863cd4939456667f9af27ff05c1850491fe3e9
Alexandre Collet <alexandre@collet.email>
2018-02-05 22:21:11 +0100

    Rename some settings and associated environment variables

--- main.py
+++ main.py
@@ -11,4 +11,4 @@ bot.add_cog(AccuWeatherCommand(bot, settings.ACCUWEATHER_API_KEY, settings.LANGU
 async def on_ready():
     print('Bot launched')
 
-bot.run(settings.DISCORD_LOGIN_TOKEN)
+bot.run(settings.DISCORD_TOKEN)
