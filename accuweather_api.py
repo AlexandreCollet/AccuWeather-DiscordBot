43e588f99b0073e0b7f6c773a59c95d316090a0f
Alexandre Collet <alexandre@collet.email>
2018-02-05 21:30:58 +0100

    AccuWeatherAPI: add _get_request method

--- accuweather_api.py
+++ accuweather_api.py
@@ -5,17 +5,21 @@ class AccuWeatherAPI:
     API_ROOT = 'http://dataservice.accuweather.com'
     API_VERSION = 'v1'
 
-    def __init__(self, api_key, language='fr'):
+    def __init__(self, api_key, language=None):
         self.api_key = api_key
         self.language = language
 
+    def _get_params(self, **additional_params):
+        params = {'apikey': self.api_key}
+
+        if self.language is not None:
+            params['language'] = self.language
+
+        return {**params, **additional_params}
+
     async def get_current_conditions(self, location_key):
         url = self.API_ROOT + '/currentconditions/v1/' + location_key
-        params = {
-            'apikey': self.api_key,
-            'details': "true",
-            'language': self.language
-        }
+        params = self._get_params(details='true')
 
         async with ClientSession() as session:
             async with session.get(url, params=params) as response:
