672cff1d23e4eb5822e6de28f1f27fdbf1983a14
Alexandre Collet <alexandre@collet.email>
2018-02-05 21:17:12 +0100

    Refactor AccuWeatherCommand: move get_*_message into utils.py

--- accuweather_command.py
+++ accuweather_command.py
@@ -3,6 +3,7 @@ import datetime
 from discord.ext import commands
 
 from accuweather_api import AccuWeatherAPI
+from utils import display_weather, display_temperature, display_wind
 
 
 class AccuWeatherCommand:
@@ -10,44 +11,17 @@ class AccuWeatherCommand:
         self.bot = bot
         self.accuweather_api = AccuWeatherAPI(accuweather_api_key)
 
-    def get_weather_message(self, condition):
-        kwargs = {
-            'emoji': '\u2600',
-            'weather': condition['WeatherText'],
-        }
-        return '{emoji} {weather}'.format(**kwargs)
-
-    def get_temperature_message(self, condition):
-        kwargs = {
-            'emoji': '\U0001F321',
-            'value': condition['Temperature']['Metric']['Value'],
-            'unit': condition['Temperature']['Metric']['Unit'],
-        }
-        return '{emoji} {value}°{unit}'.format(**kwargs)
-
-    def get_wind_message(self, condition):
-        kwargs = {
-            'emoji': '\U0001F4A8',
-            'value': condition['Wind']['Speed']['Metric']['Value'],
-            'unit': condition['Wind']['Speed']['Metric']['Unit'],
-            'value_gust': condition['WindGust']['Speed']['Metric']['Value'],
-            'unit_gust': condition['WindGust']['Speed']['Metric']['Unit'],
-        }
-        return '{emoji} {value}{unit} {value_gust}{unit_gust}'.format(**kwargs)
-
-    async def get_current_conditions_message(self, location_key):
+    @commands.command(pass_context=True, no_pm=True)
+    async def current_conditions(self, ctx, *, location_key:str):
         conditions = await self.accuweather_api.get_current_conditions(location_key)
         condition = conditions[0]
 
-        return ' '.join([
-            self.get_weather_message(condition),
-            self.get_temperature_message(condition),
-            self.get_wind_message(condition),
+        message = ' '.join([
+            display_weather(condition),
+            display_temperature(condition),
+            display_wind(condition),
         ])
 
-    @commands.command(pass_context=True, no_pm=True)
-    async def current_conditions(self, ctx, *, location_key:str):
-        message = await self.get_current_conditions_message(location_key)
         print(datetime.datetime.now(), message)
 
         await self.bot.delete_message(ctx.message)
